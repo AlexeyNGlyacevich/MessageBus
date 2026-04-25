a76285a6cb8dbb8b62ed89c44cd09a334d3a69af
AlexeyNGlyacevich <nilitmir@yandex.ru>
2026-04-25 11:25:09 +0300

    Fix UpdateConfiguration, Add MessageBusService

--- Common/AppConfigurationExtension.cs
+++ Common/AppConfigurationExtension.cs
@@ -1,6 +1,5 @@
 ﻿using MessageBusExample.ViewModels;
 using MessageBusExample.Views;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -10,12 +9,8 @@ namespace MessageBusExample.Common
 {
     public static class AppConfigurationExtension
     {
-
         public static HostApplicationBuilder ConfigureApplication(this HostApplicationBuilder builder)
         {
-            // App Configuration
-            builder.Configuration.AddJsonFile("appsettings.json", optional: false);
-
             // SerilogConfiguration
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
@@ -23,8 +18,7 @@ namespace MessageBusExample.Common
 
 
             builder.Logging.ClearProviders();
-            builder.Logging.AddSerilog();
-
+            builder.Logging.AddSerilog(Log.Logger);
 
             return builder;
         }
@@ -48,14 +42,13 @@ namespace MessageBusExample.Common
 
         public static HostApplicationBuilder ConfigureUI(this HostApplicationBuilder builder)
         {
-            // UI, Views, ViewModels.
-
             var services = builder.Services;
 
-            services.AddTransient<MainWindow>();
+            // UI, Views.
+            services.AddSingleton<MainWindow>();
 
-
-            services.AddTransient<MainWindowViewModel>();
+            // .. ViewModels.
+            services.AddSingleton<MainWindowViewModel>();
 
 
             return builder;
